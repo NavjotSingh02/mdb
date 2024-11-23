d08d908054ac276317341d53da3fd642b00c7d0c
NavjotSingh02 <navjot.singh02@edu.sait.ca>
2024-11-23 00:16:36 -0700

    dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/header";
+import Provider from "./provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,8 +25,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <Provider>
        <Header />
         {children}
+        </Provider>
       </body>
     </html>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
+
+
 import MenuItem from './MenuItem'
 import { AiFillHome } from "react-icons/ai";
 import { BsFillInfoCircleFill } from "react-icons/bs";
 import Link from 'next/link';
+import Darkmode from './Darkmode';
 
 export default function Header() {
+  
   return (
     <div className='flex justify-between items-center p-3 max-w-6xl mx-auto' >
      <div className='flex gap-4'>
         <MenuItem title='Home' address="/" Icon={AiFillHome}/>  
         <MenuItem title='about' address="/about" Icon={BsFillInfoCircleFill}/> 
      </div>
+     <div className='flex items-center gap-4'>
+      <Darkmode/>
      <Link href={"/"}>
           <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>MDB</span>
         </Link>
     </div>
-     
+     </div>
   );
 }
